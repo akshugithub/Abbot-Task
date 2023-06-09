616a62977af4ae02520d5592ffb9709791473cf4
Akshaya M <111352992+akshugithub@users.noreply.github.com>
2023-06-09 11:32:15 +0530

    Update variables.tf

--- modules/my_vpc/variables.tf
+++ modules/my_vpc/variables.tf
@@ -1,1 +1,19 @@
+# environment variables
+variable "project_name" {}
+variable "environment" {}
 
+# vpc variables
+variable "vpc_cidr" {}
+variable "instance_tenancy" {}
+
+# public subnet variables
+variable "pubsub_cidr" {}
+
+# public route tale variable
+variable "pubroute_cidr" {}
+
+# private subnet variables
+variable "privsub_cidr" {}
+
+#priv route table variable
+variable "privroute_cidr" {}
